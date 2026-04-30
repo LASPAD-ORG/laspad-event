deb1e91f6a5812d541fbbbba04eb380e66abaa8e
Papa Seydou WANE <129470066+SeydouWane@users.noreply.github.com>
2026-04-30 16:19:21 +0000

    Update site_settings to use getattr for defaults

--- config/context_processors.py
+++ config/context_processors.py
@@ -1,8 +1,7 @@
 from django.conf import settings
 
-
 def site_settings(request):
     return {
-        'SITE_NAME': settings.SITE_NAME,
-        'SITE_URL': settings.SITE_URL,
+        'SITE_URL':  getattr(settings, 'SITE_URL', 'https://events.laspad.org'),
+        'SITE_NAME': getattr(settings, 'SITE_NAME', 'LASPAD Event'),
     }
